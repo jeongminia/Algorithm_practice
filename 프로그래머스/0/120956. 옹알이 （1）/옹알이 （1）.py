4971ee6f1bcb7df66cfcb140ce892b4ee96e0f89
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2024-08-17 22:45:01 +0900

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -3,18 +3,12 @@ from itertools import permutations
 def solution(babbling):
     answer = 0
     say_lst = []
-    for j in range(len(babbling)):
+    for j in range(5):
         for i in permutations(["aya", "ye", "woo", "ma"], j):
             say_lst.append("".join(i))
-
- #   print(say_lst)
     
     while babbling:
-        a = babbling.pop()
-  #      print(a)
-        if a in say_lst:
+        if babbling.pop() in say_lst:
             answer += 1
             
-    
-    
     return answer
