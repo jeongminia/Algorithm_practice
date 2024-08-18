bbccbdb7874b5eeecbccac90b971da00d8c56f47
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2024-08-18 15:19:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 7319.42 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def solution(s):
+    answer_str = []
+    answer = []
+    
+    q = deque([(priority, idx) for idx, priority in enumerate([s[i] for i in range(len(s))])])
+    print(q)
+    
+    while q:
+        k = q.popleft()
+        
+        if k[0] not in set(answer_str):
+            answer.append(-1)
+        else:
+           # print(answer_str)
+            x = k[1] - max(list(filter(lambda x: answer_str[x] == k[0], range(len(answer_str)))))
+      #      
+   #         print(x)
+            answer.append(x)
+        
+        answer_str.append(k[0])
+
+
+    return answer
