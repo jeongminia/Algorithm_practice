e66f8391ed04de346c31a06f48543cb4d7e72169
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-01-02 21:20:46 +0900

    [level 1] Title: 기사단원의 무기, Time: 2407.58 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,24 +1,15 @@
-def cal_num(num):
-    cnt = 0
-    i = 1
-    while i * i <= num:
-        if num % i == 0:
-            cnt += 2  # i와 num // i는 서로 다른 약수
-            if i * i == num:
-                cnt -= 1  # 제곱수인 경우 중복된 약수 제거
-        i += 1
-    return cnt
-        
+
+
 def solution(number, limit, power):
-    answer = 0
-    lst = [cal_num(num) for num in range(1, number+1)]
-	
-    if limit >= max(lst):
-        return sum(lst)
-    else:
-        for j in lst:
-            if j <= limit:
-                answer +=j
-            else:
-                answer += power
-        return answer
+	lst = 0
+  #  print(number)
+  #  print([num for num in range(1, len(number)+1)])
+    
+    for num in range(1, number+1):
+        lst.append(cal_num(num))
+	    
+	#print(lst)
+    
+    
+    #answer = 0
+    return answer
