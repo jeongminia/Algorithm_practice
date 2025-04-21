02a53474eb9c2c156c6af586f0009a2c59b3eb6a
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-04-21 17:19:16 +0900

    [Silver III] Title: 반전 요세푸스, Time: 380 ms, Memory: 249504 KB -BaekjoonHub

--- 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
@@ -1,33 +1,24 @@
-import sys
 from collections import deque
 
-n, k, m = map(int, sys.stdin.readline().split())
-arr = deque([i for i in range(1, n + 1)])
-rev = False
-rev_count = 0
-count = 0
+n, k, m = map(int, input().split())
 
-while arr:
-    if len(arr) == 1:
-        print(arr.popleft())
+cnt = 0
+direction = 1 
+lst = deque(range(1, n+1)) 
+
+while lst:
+    if direction == 1:
+        for i in range(k-1):
+            lst.append(lst.popleft())
+        print(lst.popleft())
+
     else:
-        if rev:
-            tmp = arr.pop()
-            count += 1
-        else:
-            tmp = arr.popleft()
-            count += 1
-        
-        if count == k:
-            print(tmp)
-            rev_count += 1
-            count = 0
-        else:
-            if rev:
-                arr.appendleft(tmp)
-            else:
-                arr.append(tmp)
-        
-        if rev_count == m:
-            rev = not rev
-            rev_count = 0
+        for i in range(k-1):
+            lst.appendleft(lst.pop())
+        print(lst.pop())
+    
+    cnt += 1
+
+    if cnt == m: 
+        direction *= -1
+        cnt = 0 
