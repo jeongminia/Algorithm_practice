74be4ce999b1d04c5715ac1b5591cf60d945865e
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-05-09 11:51:02 +0900

    [Silver III] Title: 수강신청, Time: 464 ms, Memory: 92404 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+k, l = map(int, input().split())
+students = [input().strip() for _ in range(l)]
+
+# 인덱스 기준으로 설정해서 마지막 신청만 남김
+student_dict = {}
+for idx, student in enumerate(students):
+    student_dict[student] = idx 
+
+# 학생 신청 순서대로 정렬(인덱스 순서로)
+sorted_students = sorted(student_dict.items(), key=lambda x: x[1])
+
+# 선착순 k명 출력
+cnt = 0
+for sid, _ in sorted_students:
+    print(sid)
+    cnt += 1
+    if cnt == k:
+        break
