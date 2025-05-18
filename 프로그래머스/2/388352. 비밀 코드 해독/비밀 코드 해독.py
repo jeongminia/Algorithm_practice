7968c4b6358ee76011667c95d67f8d0ed1f750e4
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-05-18 10:15:06 +0900

    [level 2] Title: 비밀 코드 해독, Time: 200.53 ms, Memory: 21.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,9 @@
+import itertools 
+
+def solution(n, q, ans):
+    f = list(itertools.combinations(range(1, n + 1), 5))
+
+    for g, cnt in zip(q, ans):
+         f = [code for code in f if len(set(code) & set(g)) == cnt]
+
+    return len(f)
