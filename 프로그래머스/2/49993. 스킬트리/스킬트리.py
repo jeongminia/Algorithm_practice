e02afb774a04a528e7b45da57b57e8e58fd8377a
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-06-27 11:10:12 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(skill, skill_trees):
+    answer = 0
+
+    for item in skill_trees:
+        check = deque(skill)
+        
+        for i in item:
+            if i in skill:
+                k = check.popleft()
+                #print(i, k)
+                if i != k:
+                    break
+        else:
+            answer += 1
+
+        
+
+    return answer
