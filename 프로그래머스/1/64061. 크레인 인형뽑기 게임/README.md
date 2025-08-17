ffa04ef7bdc40da1e9d236e841be4b1c65955a8b
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2025-08-17 15:54:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.27 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,6 +1,6 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 17일 15:53:35
+2025년 08월 17일 15:54:05
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,30 +1,3 @@
-'''
-board: 게임 화면의 격자의 상태가 담긴 2차원 배열
-moves: 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열
-
-'''
-
 def solution(board, moves):
     answer = 0
-    n = len(board)
-    
-    temp = []
-    for now in moves:
-        now = now-1
-        
-        for i in range(n):
-            if board[i][now] != 0:
-                temp.append(board[i][now])
-                board[i][now] = 0
-                break
-        
-        if len(temp) > 1:
-            if temp[-1] == temp[-2]:
-                temp.pop()
-                temp.pop()
-                answer += 2
-     #   print(temp)
-            
-    
-    
     return answer
