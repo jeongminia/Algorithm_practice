b5eb6f3c20465721f619afb3c1c71c2984ac19ef
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-08 15:08:57 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,20 +2,24 @@ def solution(progresses, speeds):
     answer = []
     days = []
     
-    for i in range(len(speeds)):
-        n = round((100 - progresses[i])/speeds[i], 2)
-        days.append(int(n) if n == int(n) else int(n) + 1)
+    for i in range(len(progresses)):
+        if (100-progresses[i])%speeds[i] != 0:
+            k = (100-progresses[i])//speeds[i] + 1
+        else:
+            k = (100-progresses[i])//speeds[i]
+        days.append(k)
+#    print(days)
+  #  for j in range(1,len(days)):
+    for j in days:
+        if answer == []:
+            answer.append(1)
+            max_days = j
+        else:        
+            if j <= max_days:
+                answer[-1]+=1
+            else:
+                answer.append(1)
+                max_days = j
+       # print(j, max_days, answer)
     
-    while days:
-        current_day = days.pop(0)
-        print(current_day)
-        count = 1
-        
-        # 같은 배포일에 배포할 기능들 묶기
-        while days and days[0] <= current_day:
-        #    print(current_day, '>')
-        #    print(days[0])
-            days.pop(0)
-            count += 1
-        answer.append(count)
-    return answer
+    return answer # 각 배포마다 몇 개의 기능이 배포되는지
