fd36c5bc5f1d4edad2e9b3d1f3775713edee6170
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-08 15:57:42 +0900

    [level 2] Title: 프로세스, Time: 0.99 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,23 +1,21 @@
 from collections import deque
 
 def solution(priorities, location):
-    
-    # 값 복사 :  인덱스 값 저장해서 위치 도출 [[2, 0], [1, 1], [3, 2], [2, 3]]
-    q = deque([(priority, idx) for idx, priority in enumerate(priorities)])
-    
-    # 값 크기대로 정렬 [3 2 2 1]
-    #p = deque(sorted(priorities, reverse = True))
-    
-    answer = 0 
-    
+    q = deque(priorities)
+    answer = 0
     while q:
-        current = q.popleft()
-        # 현재 프로세스보다 우선순위가 높은 프로세스가 큐에 남아 있는지 확인
-        if any(current[0] < item[0] for item in q):
-            q.append(current)  # 현재 프로세스를 다시 큐에 넣음
-        else:
-            answer += 1  # 프로세스를 실행
-            if current[1] == location:  # 몇번째 실행인지 반환
-                return answer
-
+        m = max(q)
+        l = q.popleft()
+        location -= 1 
+        
+        if l != m: # 최댓값과 가장 앞에 위치한 프로세스가 일치하지 않는다면
+            q.append(l)
+            # 내 타겟이 맨 뒤로 이동한다면
+            if location < 0:
+                location = len(q) -1 
+        else: # 최댓값과 가장 앞에 위치한 프로세스가 일치한다면
+            answer += 1
+            if location < 0:
+                break
+        
     return answer
