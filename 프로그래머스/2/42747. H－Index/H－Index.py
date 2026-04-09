b2f05c58eccace6deea97ef3b976d855e8d518b5
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-09 14:22:00 +0900

    [level 2] Title: H-Index, Time: 0.21 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,12 @@
 def solution(citations):
-    citations = sorted(citations, reverse=True)
+    # 1. 내림차순 정렬
+    citations.sort(reverse=True)
     
-    for i, citation in enumerate(citations):
-        if citation <= i + 1:
-            return i  # 순번을 반환
-    
-    return len(citations)  # 모든 논문이 순번 이상으로 인용된 경우
-
-        
+    # 2. 순번(i+1)과 인용 횟수(cit) 비교
+    for i, cit in enumerate(citations):
+        # 인용 횟수가 현재 논문 편수(i+1)보다 작아지는 순간
+        if cit < i + 1:
+            return i
+            
+    # 모든 논문의 인용 횟수가 전체 논문 편수보다 크다면
+    return len(citations)
