180a99cf3e274aa3be67186c4dee0ba958440663
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-09 19:56:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 84.49 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,17 +2,17 @@ def solution(number, k):
     stack = []
     
     for num in number:
-        while (k > 0) and (stack) and (stack[-1] < num):
-    #        print(stack)
-            stack.pop() # 작은 숫자 제거
+        # 스택에 값이 있고, 지울 기회가 남았고, 현재 숫자가 스택 끝보다 크면 제거
+        while (stack) and (k > 0) and (stack[-1]<num):
+            stack.pop()
             k -= 1
+        
+        # 2. 현재 숫자를 스택에 쌓음
         stack.append(num)
+      #  print(stack)
     
-  #  print(k)
-   
-    # 이미 정렬이 다 되어 있어서 소거를 못시켰다면 .. 
-    # 내림차순으로 정렬되어 있어서 작은 숫자를 제거할 기회
+    # 리스트가 내림차순으로 정렬되어 있어서 불가피하게 아무것도 지우지 못한 케이스를 대비하여 그냥 슬라이스
     if k > 0:
         stack = stack[:-k]
-    
-    return ''.join(stack)
+        
+    return "".join(stack)
