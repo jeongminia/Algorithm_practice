fa992043c8e9fcc8a4bb5967223df4c9f1df03bc
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-09 20:12:59 +0900

    [level 2] Title: 구명보트, Time: 8.78 ms, Memory: 9.65 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,20 @@
 def solution(people, limit):
-    answer = len(people)
-    people.sort()  
-    i = 0  
-    j = len(people) - 1  
-      
-    while i < j:  
-        if people[i] + people[j] <= limit: 
-            answer -= 1  
-            i += 1  # 가벼운 사람을 다음 사람으로 이동
-        j -= 1  # 무거운 사람은 항상 보트에 태움
+    people.sort() 
     
-    return answer 
+    start = 0                 
+    end = len(people) - 1     
+    boat = 0
+    
+    while start <= end:
+        # 가장 무거운 사람과 가장 가벼운 사람을 같이 태울 수 있는지 확인
+        if people[start] + people[end] <= limit:
+            # 같이 탈 수 있으면 가벼운 사람도 출발(인덱스 증가)
+            start += 1
+            
+        # 무거운 사람은 무조건 보트에 탐
+        end -= 1
+        
+        # 보트 한 대 추가
+        boat += 1
+            
+    return boat
