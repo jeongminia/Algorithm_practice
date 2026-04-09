13f9dad93c4cd3da116f3c6231a9f837ca0bfce5
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-09 15:54:47 +0900

    [level 2] Title: 전화번호 목록, Time: 103.75 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,8 @@
 def solution(phone_book):
     answer = True
-    phone_book2 = {}
-    for i in phone_book:
-        phone_book2[i] = len(i)
-        
-    # 딕셔너리 정렬
-    phone_book = list(dict(sorted(phone_book2.items(), key=lambda x: x[1])).keys())
+    
     phone_book.sort()
+  #  print(phone_book)
     
     # while 제거하고 for 문으로 하나씩 이동해주기
     current = phone_book[0]
