47f5f7fbe56c878e38892cfa1e78f1240b8d09dc
Jeongmin Ahn <109460178+jeongminia@users.noreply.github.com>
2026-04-10 14:10:49 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,22 +1,33 @@
 def solution(n, lost, reserve):
-    students = [1] * n
+    answer = 0
     
-    for r in reserve:
-        students[r-1] += 1
+    # 각 학생마다 체육복 보유 수
+    students = [1]*n
     for l in lost:
-        students[l-1] -= 1
-        
-    # 체육복 빌려주기 순회
+        students[l-1]-=1
+    for r in reserve:
+        students[r-1]+=1
+    print(students)
+    
+    
     for i in range(n):
-        if students[i] == 0: # 체육복이 없는 학생이라면
-            # 왼쪽 학생(i-1)에게 먼저 빌려본다
-            if i > 0 and students[i-1] == 2:
-                students[i-1] -= 1
-                students[i] += 1
-            # 왼쪽 안 되면 오른쪽 학생(i+1)에게 빌려본다
-            elif i < n-1 and students[i+1] == 2:
-                students[i+1] -= 1
-                students[i] += 1
-                
-    # 체육복이 1벌 이상인 학생 수 카운트
-    return len([s for s in students if s >= 1])
+        if (students[i]==0):
+            if i==0 and (students[i+1]==2):
+                students[i+1]-=1
+                students[i]+=1
+            if i>0:
+                if (i!=n-1) and (students[i-1]==2):
+                        students[i-1]-=1
+                        students[i]+=1
+                elif (i!=n-1) and (students[i+1]==2):
+                        students[i+1]-=1
+                        students[i]+=1
+                elif (i==n-1) and (students[i-1]==2):
+                    students[i-1]-=1
+                    students[i]+=1
+    
+    # 체육수업을 들을 수 있는 학생 수
+    for a in students:
+        if a>0:
+            answer+=1
+    return answer
